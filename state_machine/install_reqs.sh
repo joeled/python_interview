9f02596ae2731adc70601e6a45bb85bd1f16f3b5
Joe <joseph@orderlyhealth.com>
2021-10-20 11:12:53 -0400

    pre post reqs scripts

--- state_machine/install_reqs.sh
+++
@@ -1,7 +0,0 @@
-brew install pyenv
-pyenv install 3.9.0
-pyenv global 3.9.0
-pip install virtualenv
-python -m venv venv
-source venv/bin/activate
-pip install pytest
---
+++ state_machine/install_reqs_pre.sh
@@ -0,0 +1,4 @@
+brew install pyenv
+pyenv install 3.9.0
+pyenv global 3.9.0
+pip install virtualenv
